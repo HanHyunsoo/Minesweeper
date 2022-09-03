062e2777061fce2d903c470478d546c7560e91a0
HanHyunSoo <gustn4563@gmail.com>
2022-09-04 04:27:13 +0900

    feat : Add bfs function

--- src/model/MinesweeperBoard.java
+++ src/model/MinesweeperBoard.java
@@ -1,6 +1,10 @@
 package model;
 
+import view.MinesweeperBoardView;
+
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 
 import static common.Constant.*;
@@ -10,13 +14,41 @@ public class MinesweeperBoard {
     private int[][] visited;
     private DifficultyType difficulty;
     private HashSet<Point> mineSet = new HashSet<>();
+    private int visitedCount;
+    private int totalMineLess;
 
     public MinesweeperBoard(DifficultyType difficulty) {
         this.difficulty = difficulty;
-        matrix = new int[difficulty.getRowSize()][difficulty.getColumnSize()];
-        visited = new int[difficulty.getRowSize()][difficulty.getColumnSize()];
+        this.matrix = new int[difficulty.getRowSize()][difficulty.getColumnSize()];
+        this.visited = new int[difficulty.getRowSize()][difficulty.getColumnSize()];
         setRandomMines();
         mineSet.forEach(this::setCountAroundMine);
+        this.visitedCount = 0;
+        this.totalMineLess = difficulty.getRowSize() * difficulty.getColumnSize() - difficulty.getMineSize();
+    }
+
+    public void bfs(Point point) {
+        Queue<Point> pointQueue = new LinkedList<>();
+        pointQueue.add(point);
+
+        while (!pointQueue.isEmpty()) {
+            Point pollPoint = pointQueue.poll();
+            if (pollPoint.isVisited(this)) continue;
+
+            visited[pollPoint.getY()][pollPoint.getX()] = 1;
+            visitedCount++;
+
+            if (pollPoint.getCellType(this) == CellType.NUMBER) continue;
+
+            for (int i = 0; i < 8; i++) {
+                Point nearPoint = new Point(pollPoint.getY() + moveY[i], pollPoint.getX() + moveX[i]);
+                if (!nearPoint.isOutBoardRange(this)) {
+                    if (nearPoint.getCellType(this) != CellType.MINE && visited[nearPoint.getY()][nearPoint.getX()] == 0) {
+                        pointQueue.add(nearPoint);
+                    }
+                }
+            }
+        }
     }
 
     private void setRandomMines() {
@@ -54,7 +86,7 @@ public class MinesweeperBoard {
             Point nearPoint = new Point(minePoint.getY() + moveY[i], minePoint.getX() + moveX[i]);
             // 좌표가 범위에 있고 해당 좌표에 지뢰가 없다면
             if (!(nearPoint.isOutBoardRange(this))) {
-                if (!(nearPoint.isExistMine(this))) {
+                if (nearPoint.getCellType(this) != CellType.MINE) {
                     matrix[nearPoint.getY()][nearPoint.getX()] += 1;
                 }
             }
@@ -75,5 +107,13 @@ public class MinesweeperBoard {
 
     public HashSet<Point> getMineSet() {
         return mineSet;
+    }
+
+    public int getVisitedCount() {
+        return visitedCount;
+    }
+
+    public int getTotalMineLess() {
+        return totalMineLess;
     }
 }
