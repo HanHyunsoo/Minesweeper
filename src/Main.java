5aa00242e62db7416d75e35c2bf0a202346bc531
HanHyunSoo <gustn4563@gmail.com>
2022-09-27 10:43:52 +0900

    feat : Add Main

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,10 @@
-import controller.GameController;
-import controller.MenuType;
 import controller.StartMenuController;
-import model.DifficultyType;
-import model.MinesweeperBoard;
-
-import java.util.Scanner;
+import view.StartMenuControllerView;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StartMenuController startMenuController = new StartMenuController(sc);
-        MenuType menuType = startMenuController.selectMenu(true);
-        DifficultyType difficultyType = DifficultyType.EASY;
-
-        switch (menuType) {
-            case START_GAME:
-                difficultyType = startMenuController.selectDifficultyType();
-                break;
-            case EXPLAIN_RULE:
-                startMenuController.explainRole();
-                startMenuController.selectMenu(false);
-                break;
-            case QUIT:
-                startMenuController.exit();
-                break;
-        }
-
-        MinesweeperBoard minesweeperBoard = new MinesweeperBoard(difficultyType);
-        GameController gameController = new GameController(sc, minesweeperBoard);
-
-        gameController.startGame();
+        StartMenuControllerView startMenuControllerView = new StartMenuControllerView();
+        StartMenuController startMenuController = new StartMenuController(startMenuControllerView);
+        startMenuController.initController();
     }
 }
